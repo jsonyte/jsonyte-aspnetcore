5c0ceabde7b519954e94f278ea8f07d1a3a29f01
Robert Coltheart <robert.coltheart@gmail.com>
2021-06-25 16:28:36 +0800

    Initial commit

--- src/Jsonyte.AspNetCore.Example/Repositories/ArticleRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Jsonyte.AspNetCore.Example.Models;
-using Jsonyte.AspNetCore.Repository;
-
-namespace Jsonyte.AspNetCore.Example.Repositories
-{
-    public class ArticleRepository : IRepository<Article>
-    {
-    }
-}
